1c42ba802017d779ae867ea3a7cf880500ee9620
0re5ama <shahbibek10@gmail.com>
2016-08-14 21:32:06 +0900

    installation

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 #vim
 ln -s ${BASEDIR}/vimrc ~/.vimrc
-#ln -s ${BASEDIR}/vim/ ~/.vim
+ln -s ${BASEDIR}/vim/ ~/.vim
 
 #zsh
 ln -s ${BASEDIR}/zshrc ~/.zshrc
