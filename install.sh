636c0bad8f7c70c363676dcc5f4216741ad0b0d2
0re5ama <shahbibek10@gmail.com>
2016-08-14 20:59:28 +0900

    installation

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+#vim
+ln -s ${BASEDIR}/vimrc ~/.vimrc
+#ln -s ${BASEDIR}/vim/ ~/.vim
+
+#zsh
+ln -s ${BASEDIR}/zshrc ~/.zshrc
+
+#urxvt
+ln -s ${BASEDIR}/Xdefaults ~/.Xdefaults
+
+#xinitrc
+ln -s ${BASEDIR}/xinitrc ~/.xinitrc
