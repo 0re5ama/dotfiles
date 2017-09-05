cc987e72e6fdfa25d22f083af3d7ff99633cc3d8
0re5ama <shahbibek10@gmail.com>
2017-09-05 21:31:04 +0545

    fixed run in awesome

--- rc.lua
+++ rc.lua
@@ -295,9 +295,10 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
     -- Prompt
-    -- awful.key({ modkey },            "r",     function () awful.util.spawn("notify-send "..tostring(mouse.screen), false) end),
+    -- awful.key({ modkey },            "r",     function () awful.util.spawn("notify-send "..tostring(mypromptbox[1]), false) end),
     -- awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
-	awful.key({ modkey },				"r",	function() menubar.show() end),
+    awful.key({ modkey },            "r",     function () mypromptbox[1]:run() end),
+    -- awful.key({ modkey },				"r",	function() menubar.show() end),
 
     awful.key({ modkey }, "x",
               function ()
