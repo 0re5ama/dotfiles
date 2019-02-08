98bf2a30ac20e936174f19fe66384393bf0aa56b
0re5ama <shahbibek10@gmail.com>
2019-02-08 20:06:17 +0545

    removed locale from status rc.lua

--- rc.lua
+++ rc.lua
@@ -231,7 +231,7 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
+            -- mykeyboardlayout,
             wibox.widget.systray(),
             statwidget,
             -- mytextclock,
